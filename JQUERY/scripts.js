ffdf8b49712c9808e919fe19ca3bc3a2a018c475
Sergio <38168163+FundacionBlockchainChile@users.noreply.github.com>
2021-08-10 22:08:21 -0400

    Prototipo Cliqueable Added

---
+++ JQUERY/scripts.js
@@ -0,0 +1,10 @@
+// ARRAYS
+
+const arrayOfFruits = ['Pera', 'Naranja', 'Manzana', 'Platano', 'Pomelo']
+
+console.log(arrayOfFruits[1])
+console.log(arrayOfFruits[1].toUpperCase())
+
+document.querySelector(
+  '.info'
+).innerHTML = `Hola a todos, la futa es ${arrayOfFruits[1].toUpperCase()}`
